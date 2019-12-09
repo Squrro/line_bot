3f6bb7a134f6061b38ad21325fe8ead2f29c1152
squrro <andy81128935@gmail.com>
2019-12-10 04:05:30 +0800

    Image

--- utils.py
+++ utils.py
@@ -17,10 +17,7 @@ def send_text_message(reply_token, text):
 
 def send_image_url(reply_token, img_url):
     line_bot_api = LineBotApi(channel_access_token)
-    message = ImageSendMessage(
-        original_content_url = img_url
-        preview_image_url = img_url
-    )
+    message = ImageSendMessage(original_content_url = img_url,preview_image_url = img_url)
     line_bot_api.reply_message(reply_token, message)
 """
 def send_button_message(id, text, buttons):
